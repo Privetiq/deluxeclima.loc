4ca76693bc1cf16f2694e5b4ddd710274c5b5a9c
benedict <zzzbenedict3zzz@gmail.com>
2017-04-15 14:37:57 +0300

    some changes

--- wp-content/themes/deluxeclimat/header.php
+++ wp-content/themes/deluxeclimat/header.php
@@ -15,18 +15,17 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/bootstrap.min.css">
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'deluxeclimat' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'deluxeclimat' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<button class="menu-toggle hidden-lg hidden-md" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'deluxeclimat' ); ?></button>
+			<div class="hidden-sm hidden-xs"><?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?></div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
